bf7a0e332a6d4cd34cf1bc77af769642d973cb2c
marcosPlaza <marcos.plaza.gonzalez@gmail.com>
2021-02-21 13:49:05 +0100

    python-v2

---
+++ python-v2/decryptor.py
@@ -0,0 +1,8 @@
+def decrypt(key, data):
+    nonce = data[:AES.block_size]
+    tag = data[AES.block_size:AES.block_size * 2]
+    ciphertext = data[AES.block_size * 2:]
+
+    cipher = AES.new(key, AES.MODE_EAX, nonce)
+    
+    return cipher.decrypt_and_verify(ciphertext, tag)
---
+++ python-v2/encryptor.py
@@ -0,0 +1,7 @@
+from Crypto.Cipher import AES
+
+def encrypt(key, data):
+    cipher = AES.new(key, AES.MODE_EAX)
+    ciphertext, tag = cipher.encrypt_and_digest(data)
+
+    return cipher.nonce + tag + ciphertext
